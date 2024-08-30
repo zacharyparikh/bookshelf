bf35db4a522e62c201038491499042410ff0458a
Zachary Parikh <parikhzachary@gmail.com>
2024-08-29 22:13:19 -0400

    Add MUI with Pigment CSS

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
+import { withPigment } from '@pigment-css/nextjs-plugin';
+import { createTheme } from '@mui/material';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+/**
+ * @type {import('@pigment-css/nextjs-plugin').PigmentOptions}
+ */
+const pigmentConfig = {
+  theme: createTheme({
+    cssVariables: true,
+    colorSchemes: { light: true, dark: true },
+    typography: {
+      fontFamily: 'var(--font-roboto)',
+    },
+  }),
+  transformLibraries: ['@mui/material'],
+};
+
+export default withPigment(nextConfig, pigmentConfig);
