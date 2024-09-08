a8de556e96a048dadb609646dc5ed622a8343a98
Zachary Parikh <parikhzachary@gmail.com>
2024-09-08 15:35:50 -0400

    Add Tailwind CSS

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,4 @@
-import { withPigment } from '@pigment-css/nextjs-plugin';
-import { createTheme } from '@mui/material';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-/**
- * @type {import('@pigment-css/nextjs-plugin').PigmentOptions}
- */
-const pigmentConfig = {
-  theme: createTheme({
-    cssVariables: true,
-    colorSchemes: { light: true, dark: true },
-    typography: {
-      fontFamily: 'var(--font-roboto)',
-    },
-  }),
-  transformLibraries: ['@mui/material'],
-};
-
-export default withPigment(nextConfig, pigmentConfig);
+export default nextConfig
